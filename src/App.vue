c8f94bd8401c06ab715d12d22ab65ecc3bc4baa9
gilsonrusso <gilsonrusso@outlook.com>
2021-06-16 12:00:03 -0400

    add logo and component home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <v-app>
-    <v-main> 
-      <h3>Bem vindos!</h3>
-    </v-main>
+    <header>
+      <nav>
+        <a href="#"><img src="./assets/logo.png" alt="suthub" /></a>
+      </nav>
+    </header>
+    <Home />
   </v-app>
 </template>
 
 <script>
+import Home from "./views/Home.vue";
 export default {
   name: "App",
+  components: { Home },
 };
 </script>
+
+<style scoped>
+header nav {
+  height: 80px;
+  display: flex;
+  padding: 10px;
+  border-bottom: 1px solid rgba(44, 44, 44, 0.2);
+}
+
+img {
+  width: 70%;
+}
+</style>
