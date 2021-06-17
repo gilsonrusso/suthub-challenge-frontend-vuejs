9312b7237a733a41dceab123b1641ca0443c91d7
gilsonrusso <gilsonrusso@outlook.com>
2021-06-17 16:56:48 -0400

    adding form brazil into region bloc page

--- src/pages/Regions/RegionBloc.vue
+++ src/pages/Regions/RegionBloc.vue
@@ -1,21 +1,51 @@
 <template>
   <div class="container">
+    <Modal v-if="showModal">
+      <FormBrazil @cancel="cancel" :data="brazilData" />
+    </Modal>
     <div class="row">
       <h3>Region Bloc</h3>
     </div>
     <div class="row">
       <div>
-        <button></button>
+        <button @click="showModal = !showModal" class="btn">Brasil</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { brazilInfo } from "../../services/restCountriesApiServices";
+import Modal from "../../components/Modal.vue";
+import FormBrazil from "./FormBrazil.vue";
 export default {
   name: "region-bloc",
+  components: { Modal, FormBrazil },
+  data() {
+    return {
+      brazilData: "",
+      showModal: false,
+    };
+  },
+  methods: {
+    async getBrazilInfo() {
+      const { data } = await brazilInfo();
+      this.brazilData = data;
+    },
+    cancel() {
+      this.showModal = false;
+    },
+  },
+  created() {
+    this.getBrazilInfo();
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.btn {
+  background-color: var(--second-color);
+  color: #fff;
+  width: 10rem;
+}
 </style>
