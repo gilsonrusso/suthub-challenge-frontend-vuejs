6e5a23e46b36ec04b8ebf93b00c65d56f104ef5c
gilsonrusso <gilsonrusso@outlook.com>
2021-06-17 04:23:23 -0400

    add file to validate cpf

---
+++ src/config/validatorCpf.js
@@ -0,0 +1,26 @@
+
+function isValidCPF(cpf) {
+
+  const strCPF = cpf.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '');
+
+  let Soma;
+  let Resto;
+  Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11)) Resto = 0;
+  if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+  Soma = 0;
+  for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+  Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11)) Resto = 0;
+  if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+  return true;
+}
+
+export { isValidCPF }
