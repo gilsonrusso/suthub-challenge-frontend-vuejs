83fe98fe0c6b2764675c887c98f0d168ae725cf3
gilsonrusso <gilsonrusso@outlook.com>
2021-06-17 16:57:39 -0400

    adding file services countries and fuction to get data of brazil

---
+++ src/services/restCountriesApiServices.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseUrl = "https://restcountries.eu/rest/v2"
+
+const brazilInfo = async () => {
+  const response = await axios.get(`${baseUrl}/name/brazil`)
+  return response
+}
+
+export { brazilInfo }
