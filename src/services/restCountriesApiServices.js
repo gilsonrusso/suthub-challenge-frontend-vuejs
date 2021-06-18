d38730a4348b6daf92f2909176c1dd5e112f967e
gilsonrusso <gilsonrusso@outlook.com>
2021-06-17 22:17:22 -0400

    adding function to get regional bloc selected

--- src/services/restCountriesApiServices.js
+++ src/services/restCountriesApiServices.js
@@ -6,5 +6,9 @@ const brazilInfo = async () => {
   const response = await axios.get(`${baseUrl}/name/brazil`)
   return response
 }
+const getRegionalBloc = async (bloc) => {
+  const response = await axios.get(`${baseUrl}/regionalbloc/${bloc}`)
+  return response
+}
 
-export { brazilInfo }
+export { brazilInfo, getRegionalBloc }
