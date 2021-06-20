846cefd87580da2699ad84520e40e9dfaadd98bd
gilsonrusso <gilsonrusso@outlook.com>
2021-06-19 23:10:13 -0400

    commit create card

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="mx-auto px-5 py-5">
+    <slot></slot>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "card",
+};
+</script>
+
+<style scoped>
+.v-card {
+  min-height: 550px;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  
+}
+
+@media (max-width: 576px) {
+  .v-card {
+    min-width: 360px;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
